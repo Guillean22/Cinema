fc4b785ece03f82ed739bf06ee855069ba99b9ec
Guillean22 <jean.rodrigue.stella@gmail.com>
2017-03-21 10:14:17 +0100

    1st testing

--- index.php
+++ index.php
@@ -1,14 +1,19 @@
 <!DOCTYPE html>
-<html>
+<html ng-app>
 	<head>
 		<title>Cinema Database</title>
+		<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.13/angular.min.js"></script>
 	</head>
 	<body>
-		<h1>Rechercher un Film</h1>
+		<?php //require_once(); ?>
 
-		<form>
-			<input type="text" name="">
-		</form>
+		<h1><u>Rechercher un Film</u></h1>
+
+
+		<label><h3>Titre : </h3></label>
+		<input type="text" ng-model="Titre" placeholder="Enter a Movie Title here">
+
+		<h2>Le film recherché : {{Titre}} est actuellement indisponible</h2>
 
 	</body>
 </html>
