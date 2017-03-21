b54a9601dadaec20f6bb3f33a8e13cfae189a842
Guillean22 <jean.rodrigue.stella@gmail.com>
2017-03-21 10:01:48 +0100

    creation de index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Cinema Database</title>
+	</head>
+	<body>
+		<h1>Rechercher un Film</h1>
+
+		<form>
+			<input type="text" name="">
+		</form>
+
+	</body>
+</html>
