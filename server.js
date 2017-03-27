29f5622592172298f6974608e0151e102c1f3b63
Guillean22 <jean.rodrigue.stella@gmail.com>
2017-03-27 17:21:54 +0200

    Identification de l'user en GET

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 let http = require('http')
 let fs = require('fs')
+let url = require('url')
 
 let server = http.createServer()
 
 server.on('request', (request, response) => {
     console.log('il y a eu une requête')
+    response.writeHead(200)
+    let query = url.parse(request.url, true).query
+    let name = query.name === undefined ? 'anonyme' : query.name
 
-    fs.readFile('index.html', (err, data) => {
+    fs.readFile('index.html', 'utf-8', (err, data) => {
         if (err){
             response.writeHead(404)
             response.end("Ce fichier n'existe pas")
@@ -14,6 +18,7 @@ server.on('request', (request, response) => {
             response.writeHead(200, {
                 'content-type': 'text/html; charset=utf-8'
             })
+            data = data.replace('{{name}}', name)
             response.write(data)
             response.end()
         }
