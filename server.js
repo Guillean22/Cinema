bc7bc0e1186e75d50898a5f39c39629ee586443b
Guillean22 <jean.rodrigue.stella@gmail.com>
2017-03-30 19:57:17 +0200

    last update

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 let http = require('http')
 let fs = require('fs')
 let url = require('url')
+var MongoClient = require("mongodb").MongoClient
+
+MongoClient.connect("mongodb://localhost/TestNoSQL", function(error, db) {
+    if (error) return funcCallback(error);
+
+    console.log("Connecté à la base de données 'TestNoSQL'");
+})
 
 let server = http.createServer()
+console.log("Le serveur est prêt à reçevoir des requêtes")
 
 server.on('request', (request, response) => {
     console.log('il y a eu une requête')
     response.writeHead(200)
+    //console.log(url.parse(request.url, true))
     let query = url.parse(request.url, true).query
     let name = query.name === undefined ? 'anonyme' : query.name
 
